6345dcdb6c57e0285c0f6d6995df0296cbaa33f9
misaels-r <msr.elestudio@gmail.com>
2022-10-07 00:32:20 -0500

    Piso
    Hay piso.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Physics } from '@react-three/cannon';
+import {Sky} from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { AmbientLight } from 'three';
+import {Ground} from './components/Ground'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas>
+        <Sky sunPosition={[0.1,0,0]}/>
+        <ambientLight intensity={0.5}/>
+        <Physics>
+          <Ground /> 
+        </Physics>
+      </Canvas>
+    </>
   );
 }
 
