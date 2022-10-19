d1e2ecb4539829e1c38d68305379b99f58c9ee4f
jsvaldezv <jsvaldezv@gmail.com>
2022-10-19 12:30:48 -0500

    Play rain working

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,17 +1,47 @@
 import "./Start.css"
+import { useEffect, useState } from "react"
+import rain from "../assets/rainAlone.mp3"
 
-export const Start = ({started, changeStarted}) => 
+export const Start = ({ started, setStarted }) => 
 {
-	const handleClick = (e) => 
+	useEffect (() => {
+		handleAudio (started);
+	}, [started])
+
+	useEffect (() => {
+		window.addEventListener ('keydown', handleEsc);
+	}, []);
+
+	const handleClick = () => 
 	{
-		e.preventDefault();
-		changeStarted();
+		setStarted (!started);
+		console.log("Click");
 	}
+
+	const handleAudio = (inState) => 
+	{
+		let audio = document.querySelector(".soundFile");
+		console.log("Handle audio:", inState);
+
+		if (inState)
+			audio.play();
+
+		else
+			audio.pause();
+	}
+
+	const handleEsc = (event) => {
+		if (event.keyCode === 27){
+			setStarted (false);
+			console.log("Stop")
+		}
+	};
 
 	return 	(
 		<div className="start" style={{display: started ? "none" : "flex"}}>
 			<h1>Welcome</h1>
-			<button onClick={handleClick}>Start</button>
+			<button onClick = {handleClick}> Start </button>
+			<audio src = {rain} className="soundFile"></audio>
 		</div>
 	)
 
---
+++ src/components/StaticSound.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export const StaticSound = () => 
+{
+	// const [isPlaying, setPlaying] = useState(false);
+
+	// const playSound = (e) => { 
+	// 	//rain.play(); 
+	// }
+	
+	// const pauseSound = (e) => { 
+	// 	//rain.pause(); 
+	// }
+
+	return ( 
+		<div>
+			{/* <button>Play</button> */}
+			{/* <audio src={"inOneO"} ></audio> */}
+		</div>
+	)
+};
