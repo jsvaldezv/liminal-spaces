48883d01fc5a301be4be3377dcf868b3872de43c
jsvaldezv <jsvaldezv@gmail.com>
2022-10-27 16:23:35 -0500

    Presentation

--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -60,11 +60,27 @@ export const Start = ({ started, setStarted, gain }) =>
 	}, [gain])
 
 	return 	(
-		<div className="start" style={{display: started ? "none" : "flex"}}>
-			<h1>Welcome</h1>
+		<div className="start" style={{ display: started ? "none" : "flex" }}>
+			
+			{/* <h1>Welcome</h1>
 			<button onClick = {handleClick}> Start </button>
 			<audio src = {rain} className="soundFile"></audio>
+			<audio src = {scream} className="screamSound"></audio> */}
+
+			<h1>La llorona</h1>
+
+			<p>
+				La leyenda de la llorona tiene muchas versiones. Se dice que el espíritu de esta mujer no descansa 
+				y todas las noches se le oye llorar y lamentar con mucha tristeza por los alrededores de las calles 
+				de los pueblos, Cuenta la leyenda que cuando se escucha el llanto cerca, es que la llorona está lejos; 
+				pero si se escucha lejos, es que estás muy cerca y se anuncia la venida de la muerte.
+			</p>
+
+			<button onClick = {handleClick} className="button"> Start </button>
+
+			<audio src = {rain} className="soundFile"></audio>
 			<audio src = {scream} className="screamSound"></audio>
+
 		</div>
 	)
 
